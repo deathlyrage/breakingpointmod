78d4916b06752b8e4794f8e13eba74382de850dc
Deathlyrage <rotda@live.com>
2017-07-05 18:20:58 +1000

    Added New Haven

---
+++ newhaven/map_newhaven/config.cpp
@@ -0,0 +1,22 @@
+/*
+	Breaking Point Mod for Arma 3
+
+	Released under Arma Public Share Like Licence (APL-SA)
+	https://www.bistudio.com/community/licenses/arma-public-license-share-alike
+
+	Alderon Games Pty Ltd
+*/
+
+#define true	1
+#define false	0
+
+#define ReadAndWrite		0
+#define ReadAndCreate		1
+#define ReadOnly		2
+#define ReadOnlyVerified		3
+
+#include "CfgPatches.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgLocationTypes.hpp"
+#include "CfgWorlds.hpp"
+#include "CfgWorldList.hpp"
