701ea468553d34856295aa43eb138428d8210857
Th3Dilli <dilli.1822@gmx.at>
2017-12-20 20:53:24 +0100

    Updated i_House_Big_02_b_base_F in CfgHavens.hpp

--- ModSource/breakingpoint_code/Configs/CfgHavens.hpp
+++ ModSource/breakingpoint_code/Configs/CfgHavens.hpp
@@ -8742,6 +8742,22 @@ class Land_i_Stone_HouseSmall_V2_F : Land_i_Stone_HouseSmall_V1_F
 				condition="((this animationPhase 'Door_3_rot') < 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
 				statement="([this, 'Door_3_rot', 'Door_Handle_3_rot_1', 'Door_Handle_3_rot_2'] call BIS_fnc_DoorOpen)";
 			};
+			class OpenDoor_4
+			{
+				condition="((this animationPhase 'Door_4_rot') < 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_4_rot', 'Door_Handle_4_rot_1', 'Door_Handle_4_rot_2'] call BIS_fnc_DoorOpen)";
+			};
+			class OpenDoor_5
+			{
+				condition="((this animationPhase 'Door_5_rot') < 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_5_rot', 'Door_Handle_5_rot_1', 'Door_Handle_5_rot_2'] call BIS_fnc_DoorOpen)";
+			};
+			class OpenDoor_6
+			{
+				condition="((this animationPhase 'Door_6_rot') < 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_6_rot', 'Door_Handle_6_rot_1', 'Door_Handle_6_rot_2'] call BIS_fnc_DoorOpen)";
+			};
+
 			class CloseDoor_1: OpenDoor_1 
 			{
 				condition="((this animationPhase 'Door_1_rot') >= 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1) && ((this animationPhase 'Hitzone_1_hide') < 0.99999				)";
@@ -8757,6 +8773,22 @@ class Land_i_Stone_HouseSmall_V2_F : Land_i_Stone_HouseSmall_V1_F
 				condition="((this animationPhase 'Door_3_rot') >= 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
 				statement="([this, 'Door_3_rot', 'Door_Handle_3_rot_1', 'Door_Handle_3_rot_2'] call BIS_fnc_DoorClose)";
 			};
+			class CloseDoor_4: OpenDoor_4 
+			{
+				condition="((this animationPhase 'Door_4_rot') >= 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_4_rot', 'Door_Handle_4_rot_1', 'Door_Handle_4_rot_2'] call BIS_fnc_DoorClose)";
+			};
+			class CloseDoor_5: OpenDoor_5 
+			{
+				condition="((this animationPhase 'Door_6_rot') >= 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_5_rot', 'Door_Handle_5_rot_1', 'Door_Handle_5_rot_2'] call BIS_fnc_DoorClose)";
+			};
+			class CloseDoor_6: OpenDoor_6 
+			{
+				condition="((this animationPhase 'Door_6_rot') >= 0.5) && ((this getVariable ['bis_disabled_Door',0]) != 1)";
+				statement="([this, 'Door_6_rot', 'Door_Handle_6_rot_1', 'Door_Handle_6_rot_2'] call BIS_fnc_DoorClose)";
+			};
+
 			class OpenLockedDoor_1: OpenDoor_1
 			{
 				displayNameDefault="<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />";
@@ -8775,7 +8807,26 @@ class Land_i_Stone_HouseSmall_V2_F : Land_i_Stone_HouseSmall_V1_F
 				condition="((this animationPhase 'Door_3_rot') < 0.1) && ((this getVariable ['bis_disabled_Door',0]) == 1)";
 				statement="([this, 'Door_Handle_3_rot_1', 'Door_Locked_3_rot'] spawn BIS_fnc_LockedDoorOpen)";
 			};
-		};
+			class OpenLockedDoor_4: OpenDoor_4
+			{
+				displayNameDefault="<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />";
+				condition="((this animationPhase 'Door_4_rot') < 0.1) && ((this getVariable ['bis_disabled_Door',0]) == 1)";
+				statement="([this, 'Door_Handle_4_rot_1', 'Door_Locked_4_rot'] spawn BIS_fnc_LockedDoorOpen)";
+			};
+			class OpenLockedDoor_5: OpenDoor_5
+			{
+				displayNameDefault="<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />";
+				condition="((this animationPhase 'Door_5_rot') < 0.1) && ((this getVariable ['bis_disabled_Door',0]) == 1)";
+				statement="([this, 'Door_Handle_5_rot_1', 'Door_Locked_5_rot'] spawn BIS_fnc_LockedDoorOpen)";
+			};
+			class OpenLockedDoor_6: OpenDoor_6
+			{
+				displayNameDefault="<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />";
+				condition="((this animationPhase 'Door_6_rot') < 0.1) && ((this getVariable ['bis_disabled_Door',0]) == 1)";
+				statement="([this, 'Door_Handle_6_rot_1', 'Door_Locked_6_rot'] spawn BIS_fnc_LockedDoorOpen)";
+			};
+
+		}
 	};
 	class Land_i_House_Big_02_b_blue_F: i_House_Big_02_b_base_F {};
 	class Land_i_House_Big_02_b_brown_F: i_House_Big_02_b_base_F {};
