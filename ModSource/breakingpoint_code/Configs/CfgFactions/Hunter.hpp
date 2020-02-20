eaf72d9752aa72bdf6ddec14a2f3ba5958fca168
DonniePlays <60700578+DonniePlays@users.noreply.github.com>
2020-02-19 22:29:18 -0500

    Update Hunter.hpp

--- ModSource/breakingpoint_code/Configs/CfgFactions/Hunter.hpp
+++ ModSource/breakingpoint_code/Configs/CfgFactions/Hunter.hpp
@@ -7,17 +7,17 @@
   Alderon Games Pty Ltd
 */
 
-	class Hunter : BP_BaseFaction
+	class Hunter: BP_BaseFaction
 	{
 		name = "Hunter";
 		icon = "\breakingpoint_ui\icons\HunterIcon.paa";
-		
-		class Levels : Levels {
-			class Level_Base : Level_Base {
-				class Spawn {
-					//Array Of Toolbelt Items
+		class Levels: Levels
+		{
+			class Level_Base: Level_Base
+			{
+				class Spawn
+				{
 					toolbelt[] = {};
-					//Array Of Random (Clothing,Backpack,Vest)
 					primary[] = {};
 					launcher[] = {"MeleeHatchet"};
 					handgun[] = {};
@@ -25,23 +25,20 @@
 					backpack[] = {};
 					vest[] = {};
 				};
-				class Uniform : BP_BaseGear {
-					//Array Of Weapons Inside Uniform
+				class Uniform: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Uniform
 					magazines[] = {};
-					//Array Of Items Inside Uniform ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
-				class Vest : BP_BaseGear {};
-				class Backpack : BP_BaseGear {};
+				class Vest: BP_BaseGear{};
+				class Backpack: BP_BaseGear{};
 			};
-			
-			class Level_0 : Level_Base {
-				class Spawn {
-					//Array Of Toolbelt Items
+			class Level_0: Level_Base
+			{
+				class Spawn
+				{
 					toolbelt[] = {};
-					//Array Of Random (Clothing,Backpack,Vest)
 					primary[] = {};
 					launcher[] = {"MeleeHatchet"};
 					handgun[] = {};
@@ -49,23 +46,20 @@
 					backpack[] = {};
 					vest[] = {};
 				};
-				class Uniform : BP_BaseGear {
-					//Array Of Weapons Inside Uniform
+				class Uniform: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Uniform
 					magazines[] = {};
-					//Array Of Items Inside Uniform ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
-				class Vest : BP_BaseGear {};
-				class Backpack : BP_BaseGear {};
+				class Vest: BP_BaseGear{};
+				class Backpack: BP_BaseGear{};
 			};
-			
-			class Level_1 : Level_Base {
-				class Spawn {
-					//Array Of Toolbelt Items
-					toolbelt[] = {};
-					//Array Of Random (Clothing,Backpack,Vest)
+			class Level_1: Level_Base
+			{
+				class Spawn
+				{
+					toolbelt[] = {"ItemMap"};
 					primary[] = {};
 					launcher[] = {"MeleeHatchet"};
 					handgun[] = {};
@@ -73,29 +67,25 @@
 					backpack[] = {"BP_AssaultPack_khk"};
 					vest[] = {};
 				};
-				class Uniform : BP_BaseGear  {
-					//Array Of Weapons Inside Uniform
+				class Uniform: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Uniform
 					magazines[] = {"ItemBandage","ItemBandage"};
-					//Array Of Items Inside Uniform ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
-				class Vest : BP_BaseGear {};
-				class Backpack : BP_BaseGear {
-					//Array Of Weapons Inside Backpack
+				class Vest: BP_BaseGear{};
+				class Backpack: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Backpack
 					magazines[] = {"ItemPainkiller","ItemKnife"};
-					//Array Of Items Inside Backpack ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
 			};
-			class Level_2 : Level_Base {
-				class Spawn {
-					//Array Of Toolbelt Items
-					toolbelt[] = {};
-					//Array Of Random (Clothing,Backpack,Vest)
+			class Level_2: Level_Base
+			{
+				class Spawn
+				{
+					toolbelt[] = {"ItemMap"};
 					primary[] = {};
 					launcher[] = {"MeleeHatchet"};
 					handgun[] = {};
@@ -103,30 +93,25 @@
 					backpack[] = {"BP_AssaultPack_sgg"};
 					vest[] = {};
 				};
-				class Uniform : BP_BaseGear {
-					//Array Of Weapons Inside Uniform
+				class Uniform: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Uniform
 					magazines[] = {"ItemBandage","ItemBandage"};
-					//Array Of Items Inside Uniform ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
-				class Vest : BP_BaseGear {};
-				class Backpack : BP_BaseGear 
+				class Vest: BP_BaseGear{};
+				class Backpack: BP_BaseGear
 				{
-					//Array Of Weapons Inside Backpack
 					weapons[] = {};
-					//Array Of Magazines Inside Backpack
 					magazines[] = {"ItemPainkiller","ItemKnife"};
-					//Array Of Items Inside Backpack ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
 			};
-			class Level_3 : Level_Base {
-				class Spawn {
-					//Array Of Toolbelt Items
-					toolbelt[] = {};
-					//Array Of Random (Clothing,Backpack,Vest)
+			class Level_3: Level_Base
+			{
+				class Spawn
+				{
+					toolbelt[] = {"ItemMap"};
 					primary[] = {};
 					launcher[] = {"MeleeHatchet"};
 					handgun[] = {};
@@ -134,35 +119,31 @@
 					backpack[] = {"BP_AssaultPack_mcamo"};
 					vest[] = {};
 				};
-				class Uniform : BP_BaseGear  {
-					//Array Of Weapons Inside Uniform
+				class Uniform: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Uniform
 					magazines[] = {"ItemBandage","ItemBandage"};
-					//Array Of Items Inside Uniform ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
-				class Vest : BP_BaseGear {};
-				class Backpack : BP_BaseGear {
-					//Array Of Weapons Inside Backpack
+				class Vest: BP_BaseGear{};
+				class Backpack: BP_BaseGear
+				{
 					weapons[] = {};
-					//Array Of Magazines Inside Backpack
 					magazines[] = {"ItemPainkiller","ItemKnife","ItemInfectedNeedle","ItemInfectedNeedle","ItemInfectedNeedle","ItemInfectedNeedle","ItemInfectedNeedle"};
-					//Array Of Items Inside Backpack ( Add to Spawn Toolbelt If they should be assigned )
 					items[] = {};
 				};
 			};
 		};
-		
-		class Points {
+		class Points
+		{
 			base = 0;
 			min = -3000;
 			max = 6000;
-			//Dont Include Level 0 ( Evil ) It's auto detected if < base value.
 			levels[] = {0,2000,5000};
 			class Aid
 			{
-				class Ranger {
+				class Ranger
+				{
 					food = 0;
 					drink = 0;
 					bandage = -10;
@@ -171,7 +152,8 @@
 					surgery = -25;
 					gut = 500;
 				};
-				class Nomad {
+				class Nomad
+				{
 					food = 0;
 					drink = 0;
 					bandage = -10;
@@ -180,7 +162,8 @@
 					surgery = -25;
 					gut = 250;
 				};
-				class Survivalist {
+				class Survivalist
+				{
 					food = 0;
 					drink = 0;
 					bandage = -10;
@@ -189,7 +172,8 @@
 					surgery = -50;
 					gut = 250;
 				};
-				class Outlaw {
+				class Outlaw
+				{
 					food = 0;
 					drink = 0;
 					bandage = 0;
@@ -198,7 +182,8 @@
 					surgery = 0;
 					gut = 250;
 				};
-				class Hunter {
+				class Hunter
+				{
 					food = 0;
 					drink = 0;
 					bandage = 0;
@@ -206,37 +191,73 @@
 					morphine = 0;
 					surgery = 0;
 					gut = 250;
+				};
+				class Engineer
+				{
+					food = 0;
+					drink = 0;
+					bandage = 0;
+					dressing = 0;
+					morphine = 0;
+					surgery = 0;
+					gut = 250;
+				};
+				class Undead
+				{
+					food = 0;
+					drink = 0;
+					bandage = 0;
+					dressing = 0;
+					morphine = 0;
+					surgery = 0;
+					gut = 0;
 				};
 			};
-			class Destroy {
-				class None {
+			class Destroy
+			{
+				class None
+				{
 					vehicle = 50;
 					storage = 50;
 				};
-				class Ranger {
+				class Ranger
+				{
 					vehicle = 500;
 					storage = 100;
 				};
-				class Nomad {
+				class Nomad
+				{
 					vehicle = 250;
 					storage = 100;
 				};
-				class Survivalist {
+				class Survivalist
+				{
 					vehicle = 250;
 					storage = 100;
 				};
-				class Outlaw {
+				class Outlaw
+				{
 					vehicle = 250;
 					storage = 100;
 				};
-				class Hunter {
+				class Hunter
+				{
 					vehicle = 250;
 					storage = 250;
+				};
+				class Engineer
+				{
+					vehicle = 0;
+					storage = 0;
+				};
+				class Undead
+				{
+					vehicle = 0;
+					storage = 0;
 				};
 			};
 			class Hunt
 			{
-				//Animal Classname = Points Gained / Lost
 				class Kill
 				{
 					BP_Chicken = 0;
@@ -248,7 +269,6 @@
 					BP_Dog_Outlaw = 0;
 					BP_Dog_Survivalist = 0;
 				};
-				
 				class Gut
 				{
 					BP_Chicken = 0;
@@ -261,32 +281,52 @@
 					BP_Dog_Survivalist = 0;
 				};
 			};
-			class Kill {
-				class Ranger {
+			class Kill
+			{
+				class Ranger
+				{
 					Level_0 = 50;
 					Level_1 = 50;
 					Level_2 = 50;
 					Level_3 = 50;
 				};
-				class Outlaw {
+				class Outlaw
+				{
 					Level_0 = 50;
 					Level_1 = 50;
 					Level_2 = 50;
 					Level_3 = 50;
 				};
-				class Hunter {
+				class Hunter
+				{
 					Level_0 = 50;
 					Level_1 = 50;
 					Level_2 = 50;
 					Level_3 = 100;
 				};
-				class Nomad {
+				class Nomad
+				{
 					Level_0 = 50;
 					Level_1 = 50;
 					Level_2 = 50;
 					Level_3 = 50;
 				};
-				class Survivalist {
+				class Survivalist
+				{
+					Level_0 = 50;
+					Level_1 = 50;
+					Level_2 = 50;
+					Level_3 = 50;
+				};
+				class Engineer
+				{
+					Level_0 = 50;
+					Level_1 = 0;
+					Level_2 = 0;
+					Level_3 = 0;
+				};
+				class Undead
+				{
 					Level_0 = 50;
 					Level_1 = 50;
 					Level_2 = 50;
